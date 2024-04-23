6ada173f23c2101a597b84f01066db8de91883e8
karolis915 <151562263+karolis915@users.noreply.github.com>
2024-04-23 18:04:11 +0300

    Add files via upload

---
+++ Sofware Tester Life Cycle 2 Sprint/docs/Test Scripts/e2e/picdu_register/src/main/java/GenerateUnits.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.util.Random;
+
+public class GenerateUnits {
+    public static String generateRandomEmail() {
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(1000) + "vardas@one.lt";
+
+    }
+  /* public static String generateRandomPass() {
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(1000) + "vvV@1986";
+
+    } */
+}
